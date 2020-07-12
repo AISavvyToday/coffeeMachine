789e24c78fed972bb7b8c164a2ba10a4a6233d94
Muhuri Jackson <54023611+MuhuriJSON@users.noreply.github.com>
2020-07-12 13:16:43 +0300

    Update coffeeApp.py

--- coffeeApp.py
+++ coffeeApp.py
@@ -7,16 +7,13 @@ class CoffeMaker:
 	def __init__(self,name,coffee_shop):
 		self.name = name
 		self.coffee_shop = coffee_shop
-
 	def do(self):
 		self.action=input('Write action (buy, fill, take, remaining, exit): ')
 		while True:
 			if self.action == 'buy':
 				self.coffee_type=input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
-
 				if self.coffee_type == 'back':
 					self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 				elif self.coffee_type == '1':
 					if self.water >= 250:
 						print('I have enough resources, making you a coffee!\n')
@@ -25,12 +22,9 @@ class CoffeMaker:
 						self.beans-=16
 						self.cash+=4
 						self.disposables-=1
-						
-
 					else:
 						print('Sorry, not enough water!')
 					self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 				elif self.coffee_type == '2':
 					if self.water >= 350:
 						print('I have enough resources, making you a coffee!\n')
@@ -40,11 +34,9 @@ class CoffeMaker:
 						self.beans-=20
 						self.cash+=7
 						self.disposables-=1
-
 					else:
 						print('Sorry, not enough water!')
 					self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 				elif self.coffee_type == '3':
 					if self.water >= 200:
 						print('I have enough resources, making you a coffee!\n')
@@ -54,12 +46,9 @@ class CoffeMaker:
 						self.beans-=12
 						self.cash+=6
 						self.disposables-=1
-
 					else:
 						print('Sorry, not enough water!')
-
 					self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 			elif self.action == 'fill':
 				self.water+=int(input('Write how many ml of water do you want to add: '))
 				self.milk+=int(input('Write how many ml of milk do you want to add: '))
@@ -67,7 +56,6 @@ class CoffeMaker:
 				self.disposables+=int(input('Write how many disposable cups of coffee do you want to add: '))
 				print('Done! ready')
 				self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 			elif self.action == 'remaining':
 				print('The coffee machine has: ')
 				print(self.water,'of water')
@@ -76,15 +64,12 @@ class CoffeMaker:
 				print(self.disposables,'of disposable cups')
 				print('${} of money'.format(self.cash))
 				self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 			elif self.action == 'take':
 				print('I gave you ${}'.format(self.cash))
 				self.cash = 0
 				self.action=input('Write action (buy, fill, take, remaining, exit): ')
-
 			elif self.action == 'exit':
 				break
-
 myCoffee = CoffeMaker('ballsy','starbucks')
 myCoffee.do()
 
